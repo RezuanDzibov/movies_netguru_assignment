eb20f6684ea9712d5098e99cb1f2930c5fe0b224
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-05-09 20:01:52 +0300

    Add Comment model

--- src/movies/movies/models.py
+++ src/movies/movies/models.py
@@ -5,3 +5,8 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     image = models.URLField()
     description = models.TextField()
+
+
+class Comment(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
