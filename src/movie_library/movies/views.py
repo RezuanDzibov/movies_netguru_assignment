a6c12644acd68783ecb76458e9576cacf8c92cf2
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-05-15 22:10:47 +0300

    Add response scheme to swagger

--- src/movie_library/movies/views.py
+++ src/movie_library/movies/views.py
@@ -5,7 +5,10 @@ from . import serializers, services
 
 
 class MovieCreate(views.APIView):
-    @swagger_auto_schema(request_body=serializers.MovieQuerySerializer)
+    @swagger_auto_schema(
+        request_body=serializers.MovieQuerySerializer,
+        responses={201: serializers.MovieSerializerOut()},
+    )
     def post(self, request: request.HttpRequest) -> response.Response:
         movie_serializer = services.add_movie(request_body=request.body)
         return response.Response(data=movie_serializer.data, status=status.HTTP_201_CREATED)
