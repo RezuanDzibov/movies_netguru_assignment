0ba4d4664ff68bcae3a50fd0a2b6c9f5b803616b
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-05-15 21:57:46 +0300

    Add MovieList view

--- src/movie_library/movies/urls.py
+++ src/movie_library/movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.MovieCreate.as_view(), name="create_movie"),
+    path("list/", views.MovieList.as_view(), name="list_movies"),
+    path("create/", views.MovieCreate.as_view(), name="create_movie"),
 ]
--- src/movie_library/movies/views.py
+++ src/movie_library/movies/views.py
@@ -3,13 +3,20 @@ import json
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import views, request, response, status
 
-from .serializers import MovieQuerySerializer
+from .serializers import MovieQuerySerializer, MovieListSerializer
 from . import services
 
 
 class MovieCreate(views.APIView):
     @swagger_auto_schema(request_body=MovieQuerySerializer)
     def post(self, request: request.HttpRequest) -> response.Response:
-        request_body = json.loads(request.body)
+        request_body = json.loads(request.body)  # TODO: move json.loads(request.body) to service
         movie_serializer = services.add_movie(request_body=request_body)
         return response.Response(data=movie_serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MovieList(views.APIView):
+    @swagger_auto_schema(responses={200: MovieListSerializer()})
+    def get(self, request: request.Request) -> response.Response:
+        movies_serializer = services.get_movies()
+        return response.Response(data=movies_serializer.data, status=status.HTTP_200_OK)
