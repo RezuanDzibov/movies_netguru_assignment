4bdeb1b1de82933d7f764b14857256caff1d29dc
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-05-15 22:08:35 +0300

    Refactor serializer importing

--- src/movie_library/movies/views.py
+++ src/movie_library/movies/views.py
@@ -1,19 +1,18 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import views, request, response, status
 
-from .serializers import MovieQuerySerializer, MovieListSerializer
-from . import services
+from . import serializers, services
 
 
 class MovieCreate(views.APIView):
-    @swagger_auto_schema(request_body=MovieQuerySerializer)
+    @swagger_auto_schema(request_body=serializers.MovieQuerySerializer)
     def post(self, request: request.HttpRequest) -> response.Response:
         movie_serializer = services.add_movie(request_body=request.body)
         return response.Response(data=movie_serializer.data, status=status.HTTP_201_CREATED)
 
 
 class MovieList(views.APIView):
-    @swagger_auto_schema(responses={200: MovieListSerializer()})
+    @swagger_auto_schema(responses={200: serializers.MovieListSerializer()})
     def get(self, request: request.Request) -> response.Response:
         movies_serializer = services.get_movies()
         return response.Response(data=movies_serializer.data, status=status.HTTP_200_OK)
