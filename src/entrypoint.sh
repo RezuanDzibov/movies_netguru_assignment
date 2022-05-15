0dbe0c3a94000bf46e813ff1a116a6b321be45a2
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-05-15 21:49:58 +0300

    Add makemigrations command

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,6 +5,7 @@ while ! nc -z db 5432; do
 done
 
 python movie_library/manage.py flush --no-input
+python movie_library/manage.py makemigrations
 python movie_library/manage.py migrate
 python movie_library/manage.py collectstatic --noinput
 exec "$@"
