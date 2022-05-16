ad2edc28cae5056b8f607f99fc1ced915a6813e4
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-05-16 21:44:47 +0300

    Register comment list uri

--- src/movie_library/movies/urls.py
+++ src/movie_library/movies/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("list/", views.MovieList.as_view(), name="list_movies"),
-    path("create/", views.MovieCreate.as_view(), name="create_movie"),
+    path("movies/list/", views.MovieList.as_view(), name="list_movies"),
+    path("movies/create/", views.MovieCreate.as_view(), name="create_movie"),
+    path("comments/list/", views.CommentList.as_view(), name="list_comments"),
 ]
