8aeac34e5e8a68509985eca252716298489a8ec7
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-05-30 20:01:47 +0300

    Add doc strings to services and views

--- src/movie_library/movies/services.py
+++ src/movie_library/movies/services.py
@@ -19,6 +19,14 @@ class add_movie:
         self.request_body: dict = json.loads(request_body)
 
     def _normalize_movie_data(self, data: dict) -> dict:
+        """
+        It takes a dictionary of movie data, and returns a dictionary of movie data with all the keys
+        lowercased and with underscores instead of camel case
+        
+        :param data: dict = {}
+        :type data: dict
+        :return: A dictionary of movie data.
+        """
         movie_data: dict = {}
         for key, value in data.items():
             if key.isupper():
@@ -30,6 +38,14 @@ class add_movie:
         return movie_data
 
     def _fetch_movie_data(self, data_to_fetch: dict) -> dict:
+        """
+        It takes a dictionary of movie data, makes a request to the IMDB API, and returns a dictionary of
+        movie data
+        
+        :param data_to_fetch: This is the data that we want to fetch from the API
+        :type data_to_fetch: dict
+        :return: A dictionary of movie data.
+        """
         imdb_api_response = requests.get(
             URI.substitute(
                 movie_title=data_to_fetch.get("title"),
@@ -43,16 +59,34 @@ class add_movie:
         return movie_data
 
     def _serialize_movie_data(self, movie_data: dict) -> Serializer:
+        """
+        It takes a dictionary of movie data, validates it, and returns a serializer
+        
+        :param movie_data: The data that will be serialized
+        :type movie_data: dict
+        :return: A serializer object.
+        """
         serializer = serializers.MovieSerializerIn(data=movie_data)
         serializer.is_valid(raise_exception=True)
         return serializer
 
     def _create_movie(self, movie_serializer: Serializer) -> Serializer:
+        """
+        It creates a movie and returns a serializer of the created movie
+        
+        :param movie_serializer: Serializer
+        :type movie_serializer: Serializer
+        :return: A serializer object
+        """
         movie = Movie.objects.create(**movie_serializer.validated_data)
         movie_serializer_out = serializers.MovieSerializerOut(instance=movie)
         return movie_serializer_out
 
     def execute(self) -> Serializer:
+        """
+        It fetches the movie data, serializes it, and then creates the movie
+        :return: A serializer object
+        """
         movie_data = self._fetch_movie_data(data_to_fetch=self.request_body)
         movie_data = self._serialize_movie_data(movie_data=movie_data)
         movie_serializer = self._create_movie(movie_serializer=movie_data)
@@ -60,12 +94,23 @@ class add_movie:
 
 
 def get_movies() -> Serializer:
+    """
+    It returns a serializer of all the movies in the database
+    :return: A serializer object
+    """
     movies = Movie.objects.all()
     movie_serializer = serializers.MovieListSerializer(many=True, instance=movies)
     return movie_serializer
 
 
 def get_comments(movie_id: Optional[int] = None) -> Serializer:
+    """
+    It returns a list of comments for a given movie
+    
+    :param movie_id: Optional[int] = None
+    :type movie_id: Optional[int]
+    :return: A serializer object
+    """
     if movie_id:
         comments = Comment.objects.filter(movie__id=movie_id)
     else:
@@ -75,6 +120,14 @@ def get_comments(movie_id: Optional[int] = None) -> Serializer:
 
 
 def add_comment(request_body: bytes) -> Serializer:
+    """
+    It takes a request body, validates it, creates a new comment, and returns a serialized version of
+    the comment
+    
+    :param request_body: bytes
+    :type request_body: bytes
+    :return: A serializer object.
+    """
     comment_serializer_in = serializers.CommentCreateInSerializer(data=request_body)
     comment_serializer_in.is_valid(raise_exception=True)
     comment = Comment.objects.create(**comment_serializer_in.validated_data)
--- src/movie_library/movies/views.py
+++ src/movie_library/movies/views.py
@@ -10,6 +10,9 @@ from . import services
 class MovieList(views.APIView):
     @swagger_auto_schema(responses={200: serializers.MovieListSerializer()})
     def get(self, request: request.Request) -> response.Response:
+        """
+        Returns all movies from the database
+        """
         movies_serializer = services.get_movies()
         return response.Response(data=movies_serializer.data, status=status.HTTP_200_OK)
 
@@ -20,6 +23,9 @@ class MovieCreate(views.APIView):
         responses={201: serializers.MovieSerializerOut()},
     )
     def post(self, request: request.HttpRequest) -> response.Response:
+        """
+        Creates a movie in the database
+        """
         movie_serializer = services.add_movie(request_body=request.body).execute()
         return response.Response(data=movie_serializer.data, status=status.HTTP_201_CREATED)
 
@@ -27,6 +33,9 @@ class MovieCreate(views.APIView):
 class CommentList(views.APIView):
     @swagger_auto_schema(responses={200: serializers.CommentListSerializer})
     def get(self, request: request.HttpRequest, movie_id: Optional[int] = None) -> response.Response:
+        """
+        Returns all comments or for a movie from the database
+        """
         if movie_id:
             comment_serializer = services.get_comments(movie_id=movie_id)
         else:
@@ -40,5 +49,8 @@ class CommentCreate(views.APIView):
         responses={201: serializers.CommentCreateOutSerializer()},
     )
     def post(self, request: request.HttpRequest) -> response.Response:
+        """
+        Creates a comment in the database
+        """
         comment_serializer = services.add_comment(request_body=request.body)
         return response.Response(data=comment_serializer.data, status=status.HTTP_201_CREATED)
